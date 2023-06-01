c243f05ea7d3bd6daa0b63fd9abb2a6a35c424b7
João Neto <jalves-d@student.42lisboa.com>
2023-06-01 01:52:22 +0100

    adding some files

--- srcs/requirements/mariadb/tools/dbsetup.sql
+++ srcs/requirements/mariadb/tools/dbsetup.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'jalves-d'@'%' IDENTIFIED BY 'jalves-d';
+CREATE USER IF NOT EXISTS 'jalves-d'@'%' IDENTIFIED BY 'awesome';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'jalves-d'@'%';
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'jalves-d';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootawesome';
