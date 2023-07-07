93684990b95b202a32472afbe3e64a64540d749d
João Neto <jalves-d@student.42lisboa.com>
2023-07-07 11:25:44 +0100

    fixing mariadb script

--- srcs/requirements/mariadb/tools/dbsetup.sql
+++ srcs/requirements/mariadb/tools/dbsetup.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'jalves-d'@'%' IDENTIFIED BY 'awesome';
+CREATE USER IF NOT EXISTS 'jalves-d'@'%' IDENTIFIED BY 'jalves';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'jalves-d'@'%';
-FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootawesome';
+FLUSH PRIVILEGES;
